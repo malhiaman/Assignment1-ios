e95f09344cb6a3a16ac05903079ed9f08ad04139
Student <student@CVS-R213D012.nic.bc.ca>
2018-01-11 16:07:11 -0800

    second commit

--- Assignment1/ViewController.swift
+++ Assignment1/ViewController.swift
@@ -9,7 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var operandFirst: UILabel!
+    @IBOutlet weak var Operator: UILabel!
+    @IBOutlet weak var operandSecond: UILabel!
+    
+    @IBOutlet weak var answer: UITextField!
+    
+    @IBAction func checkButton(_ sender: UIButton) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func ResetButton(_ sender: UIButton) {
+        
+        
+    }
+    
+    @IBAction func CheckButton(_ sender: UIButton) {
+        let alert = UIAlertController(title:"You got it!", message:"you won 3 times in this level", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler:nil)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alert.addAction(action)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
 
